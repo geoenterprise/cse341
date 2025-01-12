daccdc9347f56c5db38d7e749f14c4b48853ebc6
geoenterprise <jovannyrey@hotmail.com>
2025-01-11 19:09:05 -0500

    All the way to step 6

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
+// const lesson1Controller = require('../controllers/lesson1');
 
-routes.get('/', lesson1Controller.geoRoute);
-routes.get('/anaCriollo', lesson1Controller.anaRoute);
+// routes.get('/', lesson1Controller.geoRoute);
+// routes.get('/anaCriollo', lesson1Controller.anaRoute);
 
-module.exports = routes
+routes.use('/contacts', require('./contacts'));
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.port || port));
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+    
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node Running on port ${port}`)});
+  }
+});
+
+// app.listen(port, () => {console.log(`Web Server is listening at port ${port}`)});
